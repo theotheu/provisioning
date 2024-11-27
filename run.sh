8357d93a37f8077b1376056146e2378a0b5c296a
Theo Theunissen <{ID}+{username}@users.noreply.github.com>
2024-11-27 17:56:50 +0000

    22.04 initiated

--- run.sh
+++ run.sh
@@ -2,15 +2,10 @@
 
 DEBIAN_FRONTEND=noninteractive  
 
-sudo apt-get -y install software-properties-common aptitude git python-simplejson
+sudo apt-get -y install software-properties-common aptitude git 
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get -y install ansible
-
-# Create user non-interactively
-# ---> Proxmox template 16.04 is complaining about missing users <---
-# Replace ___PASSWORD___ and ___USERNAME___ with actual values
-# useradd -m -p `perl -e 'printf("%s\n", crypt($ARGV[0], "password"))' "___PASSWORD___"` -s /bin/bash ___USERNAME___
 
 cd ~
 mkdir -p code
